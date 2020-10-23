c8322376ca543fd4827ff01a8dd5a5b96777701a
Sabrina Luo <luohaojing@gmail.com>
2020-10-23 22:45:33 +0800

    feat: migration ready with config

---
+++ _config.js
@@ -0,0 +1,42 @@
+const siteMetadata = {
+  title: `小白妹妹写代码`,
+  siteUrl: 'https://hiitea.io',
+  siteName: `小白妹妹写代码`,
+  description: "Hiitea's tech blog for web development knowledge sharing",
+  author: 'HiiTea',
+  bio: 'JavaScript是世界上第二好的语言😏',
+  location: 'Global Citizen',
+  email: 'hi@hiitea.io',
+  link: 'https://hiitea.io',
+  avatar:
+    'https://avatars2.githubusercontent.com/u/5300359?s=460&u=ad93a82d45a91c8f6e70ec438d788c7e873ef6b5&v=4',
+  status: {
+    emoji: '🍓',
+    text: 'I may be slow to respond',
+    isBusy: true,
+  },
+  gcse: '015579750388200492852:dnpv83myu4a',
+  footerList: [
+    {
+      text: 'Home',
+      link: '/',
+    },
+    {
+      text: 'Github',
+      link: 'https://github.com/sabrinaluo',
+    },
+    {
+      text: 'Stack Overflow',
+      link: 'https://stackoverflow.com/users/3821392/sabrina-luo',
+    },
+  ],
+  flagCounter: {
+    url: 'https://info.flagcounter.com/mx4Y',
+    image:
+      '//s01.flagcounter.com/count/mx4Y/bg_FFFFFF/txt_000000/border_FFFFFF/columns_3/maxflags_9/viewers_3/labels_0/pageviews_1/flags_0/percent_0/',
+  },
+};
+
+module.exports = {
+  siteMetadata,
+};
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,30 @@
+const config = require('./_config');
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Theme Replica - example`,
-    siteUrl: 'https://www.example.com',
-  },
+  pathPrefix: `/tech`,
+  siteMetadata: config.siteMetadata,
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          'UA-52574938-4', // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          // optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
+      },
+    },
     {
       resolve: 'gatsby-theme-replica',
       options: {
